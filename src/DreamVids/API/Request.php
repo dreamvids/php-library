7ede06278c44350c05096ec348b81962894e8e7b
PHPeter <phpeter@phpeter.fr>
2016-09-03 15:21:09 +0200

    Stupid things, as always: variable content into a vendor...

--- src/DreamVids/API/Request.php
+++ src/DreamVids/API/Request.php
@@ -3,10 +3,6 @@ namespace DreamVids\API;
 
 class Request {
 	const API_URL = 'http://dev.box:8080/shared/DreamVids/API/';
-	const CLIENT_PUBLIC_KEY = '9b55842ccdb4aae14fda2b22098eb9e682cb2b2c9f74d638f8292b4956af583eafb6e5567d5b1192e91ce37c08cae496d4c18fdcf8f411b304ddd8593a892475';
-	const CLIENT_PRIVATE_KEY = '7746c0d84f7448c8fccc2104ee927d56c39825413296ebd30a926bcdbc70b9f2bf7081b13acdcdfc52f95019a155192d5a90d033f099f63acd207a4c1fe64852';
-	const CLIENT_NAME = 'root';
-	const CLIENT_DOMAIN = 'localhost';
 
 	private $curl;
 	private $rawResponse;
@@ -18,15 +14,15 @@ class Request {
 		$this->rawResponse = '';
 		$this->response = [];
 		$dataToHash = json_encode([
-			self::CLIENT_NAME,
-			self::CLIENT_DOMAIN,
+			Client::NAME,
+			Client::DOMAIN,
 			$uri,
 			$method,
 			$data
 		]);
 		$this->headers = [
-			'X-Public: ' . self::CLIENT_PUBLIC_KEY,
-			'X-Hash: ' . hash_hmac('sha512', $dataToHash, self::CLIENT_PRIVATE_KEY)
+			'X-Public: ' . Client::PUBLIC_KEY,
+			'X-Hash: ' . hash_hmac('sha512', $dataToHash, Client::PRIVATE_KEY)
 		];
 		if ($sessid != null)
 			$this->headers[] = 'X-Session-ID: ' . $sessid;
